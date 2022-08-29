57d92630a719e8adce429a443bbc0ad21a084f1c
Chad Hovey <hovey@me.com>
2022-08-29 14:36:32 -0600

    proof of concept: Iterable to np.ndarray type hint

--- src/atpixel/MRI_to_stl.py
+++ src/atpixel/MRI_to_stl.py
@@ -88,8 +88,8 @@ def alpha_shape_mask_slice(
 
 
 def resample_equal_voxel_mask(
-    array: Iterable, scale_ax_0: Union[float,int], scale_ax_1: Union[float,int], scale_ax_2: Union[float,int]
-) -> Iterable:
+    array: np.ndarray, scale_ax_0: Union[float, int], scale_ax_1: Union[float, int], scale_ax_2: Union[float, int]
+) -> np.ndarray:
     """Given a 3D mask and specified scaling of each axis. Will return the re-scaled mask."""
     array_mult = array * 100.0
     array_rescale = ndimage.zoom(
